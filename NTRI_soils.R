42e868c1f6b22cc08eec5e516aa048119faa3883
LesleyAtwood <33636908+LesleyAtwood@users.noreply.github.com>
2022-07-25 10:41:17 -0400

    explore raster distribution, calculate soil sample numbers using CDM and quantile sampling of raster dataset

--- NTRI_soils.R
+++ NTRI_soils.R
@@ -5,10 +5,10 @@ setwd("C:/Users/lesley.atwood/Desktop/GitHub/NTRI")
 #What this script does
 #read in raster files
 #plot raster
-#conduct unspervised kMeans classification using satelitte imagery
-#see: https://gis.stackexchange.com/questions/345823/identify-spatially-contiguous-clusters-in-raster-data-using-kmeans
-#see: https://www.gis-blog.com/unsupervised-kmeans-classification-of-satellite-imagery-using-r/
+#conducts unspurvised kMeans classification using satellite imagery
 #power analysis to determine # of samples
+#CDM eq. II to determine # of samples
+
 
 #### LOAD LIBRARIES ####
 library(rgdal)
@@ -21,9 +21,10 @@ library(ggplot2)
 library(stats)
 library(dplyr)
 library(foster)
+library(terra)
 
 
-# import raster data (clipped using QGIS)
+# import raster data (clipped using QGIS) ##########
 #https://soilgrids.org/
 
 r <- raster("soc_t..ha_0..20cm_wgs84_clipped.tif")
@@ -45,7 +46,7 @@ nr <- getValues(r)
 #set random number
 set.seed(99)
 
-######start fx #####
+######kmeans plots to determine cluster number #####
 
 wss<-numeric(10)
 for(i in 1:10){wss[i]<-sum(kmeans(na.omit(nr),centers=i)$withinss)}#sum within sum of squares and plot this
@@ -158,12 +159,12 @@ avg <- avg[complete.cases(avg)]
 sd <- std$stdev
 sd <- sd[complete.cases(sd)]
 
-#Power analysis for each strata
-#use soils revealed for intervention potential of area
+#Sample size: for big and small gains based on soils revealed data#########
 #increase of 7 and 4.26/20 years to get ton/ha/yr
 biggain <- 7/20
 smallgain <- 4.26/20
 
+#####Big Gain SOC###############
 s1_big <- power.t.test(n = NULL, delta = ((avg[1]+biggain)-avg[1]),
                sd = sd[1],
                sig.level = 0.05,
@@ -188,7 +189,7 @@ s4_big <- power.t.test(n = NULL, delta = ((avg[4]+biggain)-avg[4]),
 numsamples_0.35t_ha_yr <- c(s1_big, s2_big, s3_big, s4_big)
 
 
-###################small gain
+###################small gain SOC#####
 
 s1_small <- power.t.test(n = NULL, delta = ((avg[1]+smallgain)-avg[1]),
                        sd = sd[1],
@@ -228,7 +229,7 @@ results.df[,7] <- numsamples_0.35t_ha_yr
 results.df[,8] <- numsamples_0.213t_ha_yr
 results.df <- data.frame(results.df)
 
-#rename columns########begin here. then recalculate n using CDM equation
+#rename columns#######
 
 
 results.df <- results.df %>%
@@ -242,51 +243,127 @@ results.df <- results.df %>%
                         n.power_0.213tC.ha.yr = X8)
 
 write.csv(results.df, "NTRI_strata_results.csv", row.names = FALSE)
-results.df <- read.csv("NTRI_strata_results.csv")
-
-###use kmeans cluster results
-#results.df <- read.csv("NTRI_strata_results.csv")
-
-SamplesKm <- results.df %>% select(sd_SOC.tC.ha, mean_SOC.tC.ha, area_proportion)
 
 
-#calculating total number of sample plots required using equation 2 from CDM A/R tool for calculation of the number of sample plots for measurements 
-#within A/R CDM project activities (Version 02.1.0)
-#equation assumes that sample plot area is much smaller <5% of total project area)
 
-#t value at 95% confidence interval (t) and a  
-t = 1.960
+##################CDM calculations#####################
 
-mean.total <- mean(soc_clusters$socvalue, na.rm = TRUE)
-sd <- sd(soc_clusters$socvalue[!is.na(soc_clusters$socvalue)])
 
-se <- function(x) {sqrt(var(x[!is.na(x)])/length(x[!is.na(x)]))}
-n.total <- length(soc_clusters$socvalue[!is.na(soc_clusters$socvalue)])
-se(soc_clusters$socvalue)
 
-SamplesKm$sd_SOC.tC.ha <- SamplesKm$sd_SOC.tC.ha*100
-SamplesKm$mean_SOC.tC.ha <- SamplesKm$mean_SOC.tC.ha*100
 
-#margin of error(p)
-E = mean.total + (t*(2*sd/sqrt(n.total)))
-E = 0.35*mean.total
+#randomly sample your raster stratified across the distribution say, in 0.10 quantiles
+#use the subsample to calculate your CI’s
+#Note that if your rasters distribution is Gaussian, there is no need to stratify as a random sample should capture the sample distribution.
+#To ensure that there is nothing hinky going on you could Bootstrap this procedure and output the p-values along with a distribution of the upper and lower CI’s
+#this way you can evaluate if there is a sampling condition that is increasing the variance of the CI’s.
+#Although, this is not entirely necessary if you just check to see if the sample distribution of the subsample is the same as your population (raster).
+#I can almost guarantee that, even with a small sample fraction and using an equivalency test (eg., Kolmogorov–Smirnov), the sample distribution will match.
+
+#Randomly sample each strata in 0.10 quantiles
+
+#check distribution of data
+hist(r)
+#Gaussian
+
+
+####Calculate CI for entire area (r) using quantiles
+    soc_total <- quantile(r, probs=seq(0,1,1/10), na.rm=TRUE)
+    ###issue with quantile results and how they integrate into sample stratified, see size = .1 below
+    
+    #variables needed for CDM calculation#####
+    mean.total <- mean(soc_total)
+    sd.total <- sd(soc_total)
+    data.total <- unname(soc_total)
+    
+
+#strata 1####
+      #distribution
+      clusters_s1 <- clusters
+      clusters_s1[clusters_s1 > 1] <- NA
+      clusters1<- mask(r, clusters_s1)
+      hist(clusters1)
+      #biomodal
+      
+      #subsamples
+      soc_s1 <- quantile(clusters1, probs=seq(0,1,1/10), na.rm=TRUE)
+      mean_s1 <- mean(n.sample_s1)
+      sd_s1 <- sd(n.sample_s1)
+      
+      
+#strata 2 ####
+      #distribution
+      clusters_s2 <- clusters
+      clusters_s2[clusters_s2 != 2] <- NA
+      clusters2<- mask(r, clusters_s2)
+      hist(clusters2)
+      #skew right
+      
+      #subsamples
+      soc_s2 <- quantile(clusters2, probs=seq(0,1,1/10), na.rm=TRUE)
+      mean_s2 <- mean(n.sample_s2)
+      sd_s2 <- sd(n.sample_s2)
+
+
+#strata 3 ####
+      #distribution
+      clusters_s3 <- clusters
+      clusters_s3[clusters_s3 != 3] <- NA
+      clusters3<- mask(r, clusters_s3)
+      hist(clusters3)
+      #bimodal
+      
+      #subsamples
+      soc_s3 <- quantile(clusters3, probs=seq(0,1,1/10), na.rm=TRUE)
+      mean_s3 <- mean(n.sample_s3)
+      sd_s3 <- sd(n.sample_s3)
+
+#strata 4 ####
+      #distribution
+      clusters_s4 <- clusters
+      clusters_s4[clusters_s4 != 4] <- NA
+      clusters4<- mask(r, clusters_s4)
+      hist(clusters4)
+      #skew left
+      
+      #subsamples
+      soc_s4 <- quantile(clusters4, probs=seq(0,1,1/10), na.rm=TRUE)
+      mean_s4 <- mean(n.sample_s4)
+      sd_s4 <- sd(n.sample_s4)
+
+      
+#variables needed for CDM calculation#####
+means <- c(mean_s1, mean_s2, mean_s3, mean_s4)
+sds <- c(sd_s1, sd_s2, sd_s3, sd_s4)      
+areas <- c(strata1area, strata2area, strata3area, strata4area)
+area.prop <- areas/totalarea
+data <- c(unname(soc_s1), unname(soc_s2), unname(soc_s3), unname(soc_s4))
+
+
+
 
 ####Estimation of Sample Plots using CDM equations#######
 
 ##CDM Eq. II - CALCULATION OF NUMBER OF SAMPLE PLOTS REQUIRED FOR ESTIMATION OF C STOCK WITHIN THE PROJECT BOUNDARY
 #equation results in value < 1, use simplified equation below for projects with small sampling fraction (<5% project area)
+#t value at 95% confidence interval (t) and a  
+t = 1.960
 
-n = (t^2* sum(SamplesKm[,3]*SamplesKm[,1])^2) / 
- (E^2 + t^2 * sum(SamplesKm[,3]*SamplesKm[,1]^2))
+sd_total <- sd(data)
+n.total <- length(data)
+
+#margin of error(p)
+E = (t*(sd_total/sqrt(n.total)))
+
+
+#equation II
+n = (t^2* sum(area.prop*sds)^2) / 
+ (E^2 + t^2 * sum(area.prop*sds^2))
 #<1 results, use simplified equation
 
 
 #simplified equation, small sampling fraction (<5% of project area)
-  n = (t/E)^2*(sum(SamplesKm[,3]*SamplesKm[,1]))^2
-  #139 total samples
+  n = (t/E)^2*(sum(area.prop*sds))^2
+  #4.45891 total samples
 
 
-results.df$n.CDM <- n*((SamplesKm$area_proportion*SamplesKm$sd_SOC.tC.ha)/(sum(SamplesKm$area_proportion*SamplesKm$sd_SOC.tC.ha)))
- 
-write.csv(results.df, "NTRI_strata_results.csv", row.names = FALSE)
-
+#########################
